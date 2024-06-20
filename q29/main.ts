a9c5dc712fd5b1a4cf8b94bcf18d0160a59936ef
Huma Fahad Siddqui <167395734+HumafahadSidd@users.noreply.github.com>
2024-06-21 00:34:23 +0500

    Update main.ts

--- q29/main.ts
+++ q29/main.ts
@@ -1,5 +1,5 @@
 //Q:29
-let fvt_fruits: string[] = ["watermelon", "cherry", "grapes"]
+let fvt_fruits: string[] = ["watermelon", "cherry", "grapes","orange","pineapple"]
 
 if(fvt_fruits.includes("watermelon")) {
     console.log("I really like watermelon!")
@@ -11,3 +11,16 @@ if(fvt_fruits.includes("grapes")) {
     console.log("I really like grapes!")
 }
 else (console.log('your fruit is not in the list'));
+// Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+if(fvt_fruits.includes("watermelon")) {
+    console.log("you really like bananas!")
+}
+if(fvt_fruits.includes("cherry")) {
+    console.log("you really like bananas!")
+}
+if(fvt_fruits.includes("orange")) {
+    console.log("you really like bananas!")
+}
+if(fvt_fruits.includes("pineapple")) {
+    console.log("you really like bananas!")
+}
